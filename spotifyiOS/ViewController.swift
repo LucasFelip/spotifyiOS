7e88fd94f0591ea3449665a11d3899fce3bbcc08
Lucas Ferreira <luscafreis@gmail.com>
2023-07-04 19:50:27 -0300

    Initial Commit

---
+++ spotifyiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  spotifyiOS
+//
+//  Created by Lucas Ferreira on 04/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
